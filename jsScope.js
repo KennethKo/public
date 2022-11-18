03a23425a6eefb415c453ca5a6c531ca3df2fb4c
KennethKo <31635054+KennethKo@users.noreply.github.com>
2022-11-17 18:47:18 -0800

    fix js binding to this.
    Didn't know arrow functions didn't have access to the proper this.

--- jsScope.js
+++ jsScope.js
@@ -4,8 +4,8 @@
   `let x = y+1` is eqivalent to the debug `let x = y.inspect()+1`
 */
 
-Object.prototype.let = f => { f(this) }
-Object.prototype.apply = f => { this.let(f); this }
-Object.prototype.inspect = () => { this.apply(console.log) }
+Object.prototype.let = function(f) { f(this) }
+Object.prototype.apply = function(f) { this.let(f); this }
+Object.prototype.inspect = function() { this.apply(console.log) }
 
 export jsScope
