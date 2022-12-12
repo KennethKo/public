be1d7e815290827ef8b653c14abb990d386b6895
KennethKo <31635054+KennethKo@users.noreply.github.com>
2022-12-12 13:50:42 -0800

    removing module stuff

--- jsScope.js
+++ jsScope.js
@@ -2,10 +2,10 @@
   Bit of util for more functional js
   
   `let x = y+1` is eqivalent to the debug `let x = y.inspect()+1`
+  
+  require('./jsScope.js')
 */
 
 Object.prototype.let = function(f) { f(this) }
 Object.prototype.apply = function(f) { this.let(f); this }
 Object.prototype.inspect = function() { this.apply(console.log) }
-
-export jsScope
