850db4df885439e224bbaebf2e3f88f2349d8a87
KennethKo <31635054+KennethKo@users.noreply.github.com>
2024-09-04 12:38:50 -0700

    Adding space to join

--- bookmarklets/getEmails.js
+++ bookmarklets/getEmails.js
@@ -1,11 +1,11 @@
 // Copy the minified js below into a bookmarklet
-javascript:{r=prompt("Paste Raw Text","");t=[...new Set([...r.matchAll("[^\\s\"')\(,]+@[^ .]+\.[^\\s\"')\(,]+").map(m => m[0])])].join(',');d=document;l=e=>{d.removeEventListener("copy",l,true);e.preventDefault();c=e.clipboardData;c.clearData();c.setData("text/plain",t);};d.addEventListener("copy",l,true);d.execCommand("copy");d.preventDefault;alert('Emails Copied\n'+t);}
+javascript:{r=prompt("Paste Raw Text","");t=[...new Set([...r.matchAll("[^\\s\"')\(,]+@[^ .]+\.[^\\s\"')\(,]+").map(m => m[0])])].join(', ');d=document;l=e=>{d.removeEventListener("copy",l,true);e.preventDefault();c=e.clipboardData;c.clearData();c.setData("text/plain",t);};d.addEventListener("copy",l,true);d.execCommand("copy");d.preventDefault;alert('Emails Copied\n'+t);}
 
 
 // cross cribbed from: https://gist.github.com/stefanmaric/2abf96c740191cda3bc7a8b0fc905a7d
 // javascript:{
 //   r=prompt("Paste Raw Text","");
-//   t=[...new Set([...r.matchAll("[^\\s\"')\(,]+@[^ .]+\.[^\\s\"')\(,]+").map(m => m[0])])].join(',');
+//   t=[...new Set([...r.matchAll("[^\\s\"')\(,]+@[^ .]+\.[^\\s\"')\(,]+").map(m => m[0])])].join(', ');
 //   d=document;
 //   l=e=>{
 //     d.removeEventListener("copy",l,true);
