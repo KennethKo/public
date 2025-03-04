37048e77a6d031b180e31a4ee4a8d88d5d11c636
KennethKo <31635054+KennethKo@users.noreply.github.com>
2025-03-04 10:07:46 -0800

    Update ds.bat
    Adding a defaultable param for overriding the prompt

--- ds.bat
+++ ds.bat
@@ -4,15 +4,17 @@
 :: Quick and dirty batch script to pipe web text into my windows ollama.
 :: Must have lynx installed in your wsl (or install native lynx properly w some kind of native windows sed to trim the header and references)
 
-setlocal
+setlocal EnableDelayedExpansion
 set model=deepseek-r1:1.5b
 
 IF [%1]==[] (
     echo "Running repl on %model%"
     ollama run %model%
 ) ELSE (
-    echo "Using %model% to summarize %1"
-    wsl lynx -dump %1^| sed -n "/^\w/,/^References$/p" | ollama run %model% "Please summarize this text"
+    set p=%2
+    if not [%p%]==[] set p="Please summarize this text"
+    echo "Using %model% to prompt !p! on %1"
+    wsl lynx -dump %1^| sed -n "/^\w/,/^References$/p" | ollama run %model% !p!
 )
 
 endlocal
